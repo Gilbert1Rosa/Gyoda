e90ef4cd4c0d366a7f7091bb236dcf0a48b6b386
Gilbert Rosa <gilbertli2033@gmail.com>
2019-02-12 02:10:34 -0400

    Adding LoginService.js

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-var express = require('express');
+var express = require('express'),
+    oauthServer = require('node-oauth2-server');
+const UserService = require('./service/UserService');
+const LoginService = require('./service/LoginService');
 var app = express();
+var router = express.Router;
 
+var logger = (req, res, next) => {
+    console.log("Request received");
+    next();
+};
 
+app.use(logger);
+app.use('/', UserService());
+app.use('/', LoginService(app));
 app.get('/', (req, res) => {
     var message = "ASDF";
-    res.send(`{"message":${message}}`);
+    res.send(`{"message":"${message}"}`);
 });
-
 app.listen(5000);
--- service/LoginService.js
+++ service/LoginService.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (app) => {
-    router.get('/user', (req, res) => {
-        var message = "There's gonna be some users";
+    router.get('/login', app.auth.grant(), (req, res) => {
+        var message = "This is the login";
         res.send(`{"message":"${message}"}`);
     });
     return router;
