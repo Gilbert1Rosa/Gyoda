4329a5ee0b966fb50f1ca565897ccd3e7081852e
Gilbert Rosa <gilbertli2033@gmail.com>
2019-02-26 01:06:33 -0400

    - Implementing OAuth2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var express = require('express');
+var express = require('express'),
+    oauthServer = require('node-oauth2-server');
 const bodyParser = require('body-parser');
 const UserService = require('./service/UserService');
 const LoginService = require('./service/LoginService');
@@ -10,9 +11,18 @@ var logger = (req, res, next) => {
     next();
 };
 
+app.oauth = new oauthServer({
+    model: {},
+    grants: ['password'],
+    debug: true
+});
+
 app.use(logger);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.all('/gyoda/api/oauth', app.oauth.grant());
+
+
 app.use('/gyoda/api', LoginService(app));
 app.use('/gyoda/api', UserService(router));
 app.listen(5000);
