fed1a3ef57eeda9602ca93e70e8cc2a3c0ec52fc
Gilbert1Rosa <gilbertli2033@gmail.com>
2019-07-02 00:46:26 -0400

    - Creating project DAO

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ const UserDAO = require('./src/data/dao/UserDAO');
 const AuthService = require('./src/service/AuthService');
 const IterationService = require('./src/service/IterationService');
 const UserService = require('./src/service/UserService');
+const ProjectService = require('./src/service/ProjectService');
+const TaskService = require('./src/service/TaskService');
 
 /* Util */
 const SessionManager = require('./src/util/SessionManager');
 const OracleConnection = require('./src/util/oracle/OracleConnection');
-const Mapper = require('./src/util/oracle/OracleMapper');
 
 /* Configurations */
 var app = express();
@@ -25,7 +26,7 @@ async function initServer() {
     let connection = await OracleConnection.connect({
         user: 'GyodaDba',
         password: 'password',
-        connectString: 'localhost/xe'
+        connectString: 'gyodadb.cb277e4k1thw.us-east-2.rds.amazonaws.com/orcl'
     });
 
     var iterationDAO = new MockIterationDAO();
@@ -55,8 +56,10 @@ async function initServer() {
 
     var userService = UserService(app, router, userDAO, authService);
     var iterationService = IterationService(app, router, iterationDAO);
+    var projectService = ProjectService(app, router, {});
+    var taskService = TaskService(app, router, {});
 
-    app.use('/gyoda/api', userService, iterationService, authService.routes);
+    app.use('/gyoda/api', userService, iterationService, projectService, taskService, authService.routes);
     app.listen(5000);
 }
 
---
+++ src/data/dao/ProjectDAO.js
@@ -0,0 +1,68 @@
+const Mapper = require('../../util/oracle/OracleMapper');
+const OracleConnection = require('../../util/oracle/OracleConnection');
+
+const fields = [
+    {
+        name: 'ID',
+        objName: 'name'
+    },    
+    {
+        name: 'TITULO',
+        objName: 'title'
+    },    
+    {
+        name: 'DESCRIPCION',
+        objName: 'description'
+    }
+];
+
+module.exports = class ProjectDAO {
+    getProjects(callback) {
+        let query = `SELECT * FROM vProyectos`;
+        OracleConnection.execute(this.connection, query, {}, {}, (err, data) => {
+            if (err) { 
+                let customError = { message: `Error: ${err}`, errorCode: 2002 };
+                callback(customError, null);
+            } else {
+                let mappedData = Mapper.getRowsAsObjs(data, fields);
+                callback(null, mappedData);
+            }
+        })
+    }
+
+    addProject(project, callback) {
+        let command = `CALL CREATE_PROJECT(:title, :description)`;
+        OracleConnection.execute(this.connection, command, project, {}, (err, data) => {
+            if (err) { 
+                let customError = { message: `Error: ${err}`, errorCode: 2002 };
+                callback(customError, null);
+            } else {
+                callback(null, data);
+            }
+        });
+    }
+
+    modifyProject(project, callback) {
+        let command = `CALL MODIFY_PROJECT(:id, :title, :description)`;
+        OracleConnection.execute(this.connection, command, project, {}, (err, data) => {
+            if (err) { 
+                let customError = { message: `Error: ${err}`, errorCode: 2002 };
+                callback(customError, null);
+            } else {
+                callback(null, data);
+            }
+        });
+    }
+
+    deleteProject(projectid, callback) {
+        let command = `CALL DELETE_USER(${projectid})`;
+        OracleConnection.execute(this.connection, command, [], {}, (err, data) => {
+            if (err) { 
+                let customError = { message: `Error: ${err}`, errorCode: 2002 };
+                callback(customError, null);
+            } else {
+                callback(null, data);
+            }
+        });
+    }
+}
